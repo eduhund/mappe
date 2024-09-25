76fe5318f01325811f7b140826e8b7273a6e497a
Roman Nebel <im.roman.nebel@gmail.com>
2024-09-25 08:44:18 +0200

    Add basic map

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,4 +1,5 @@
 const MAX_SPACE = 262143;
+const FRAME_SIZE = 262;
 
 function uiMessageHandler(message) {
   console.log('Message from UI:', message);
@@ -32,24 +33,37 @@ function getMapCorners(nodes) {
 function getScale([x1, y1, x2, y2]: number[]) {
   const width = Math.abs(x2 - x1);
   const height = Math.abs(y2 - y1);
-  return Math.round(((width > height ? width : height) / MAX_SPACE) * 1000) / 1000 || 0.001;
+  return Math.round(((width > height ? width : height) / MAX_SPACE / FRAME_SIZE) * 1000) / 1000 || 0.001;
+}
+
+function calcNodesPreview(nodes, scale) {
+  return nodes.map((node) => {
+    const { id, x, y, width, height } = node;
+    return {
+      id,
+      x: Math.ceil((x + MAX_SPACE / 2) * scale),
+      y: Math.ceil((y + MAX_SPACE / 2) * scale),
+      width: Math.round(width * scale) || 1,
+      height: Math.round(height * scale) || 1,
+    };
+  });
 }
 
 function updateMap() {
   const nodes = getNodes();
   const corners = getMapCorners(nodes);
   const scale = getScale(corners);
-  console.log(scale);
-  //setMapMatrix
+  const nodesPreview = calcNodesPreview(nodes, scale);
+  figma.ui.postMessage({ type: 'updateMap', data: nodesPreview });
   //getCurrentView
   //sendMapData
 }
-
-updateMap();
 
 figma.showUI(__html__, {
   width: 262,
   height: 262,
 });
+
+updateMap();
 
 figma.ui.onmessage = uiMessageHandler;
--- src/ui/components/App.tsx
+++ src/ui/components/App.tsx
@@ -1,9 +1,42 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import '../styles/ui.css';
-import Title from 'antd/es/typography/Title';
 
 function App() {
-  return <Title level={2}>Start Your Jorney!</Title>;
+  const mapRef = useRef(null);
+
+  const [frames, setFrames] = useState([
+    { id: 'rect1', x: 50, y: 50, width: 200, height: 100 },
+    { id: 'rect2', x: 2, y: 1, width: 10, height: 15 },
+  ]);
+
+  useEffect(() => {
+    const canvas = mapRef.current;
+    const ctx = canvas.getContext('2d');
+
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    frames.forEach(({ x, y, width, height }) => {
+      console.log(x, y, width, height);
+      ctx.fillStyle = 'black';
+      ctx.fillRect(x, y, width, height);
+    });
+  }, [frames]);
+
+  useEffect(() => {
+    const handleMessage = (event) => {
+      const { type, data } = event.data.pluginMessage;
+
+      if (type === 'updateMap') {
+        setFrames(data);
+      }
+    };
+
+    window.addEventListener('message', handleMessage);
+
+    return () => window.removeEventListener('message', handleMessage);
+  }, []);
+
+  return <canvas id="map" ref={mapRef} width="262" height="262"></canvas>;
 }
 
 export default App;
