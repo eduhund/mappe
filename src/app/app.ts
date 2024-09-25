60c143ecc709b045f2fc91bc7d1b9b9d4ca228d1
Roman Nebel <im.roman.nebel@gmail.com>
2024-09-26 00:02:35 +0200

    Add relative scale logic

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,20 +1,46 @@
 const MAX_SPACE = 262143;
 const FRAME_SIZE = 262;
+const DEFAULT_CORNERS = [-131071, -131071, 131071, 131071];
+
+let corners = DEFAULT_CORNERS;
+let biggerCorner = MAX_SPACE;
+let scale = 0;
+let isRelativeScale = false;
 
 function uiMessageHandler(message) {
   console.log('Message from UI:', message);
   const { type, data } = message;
   if (type === 'CHANGE_RELATIVE_SCALE') {
     const { value } = data;
-    updateMap(value);
+    setIsRelativeScale(value);
+    updateMap();
   }
+}
+
+function calcScale(currentCorner) {
+  return FRAME_SIZE / currentCorner;
+}
+
+function setScale(currentCorner) {
+  scale = calcScale(currentCorner);
+  return scale;
+}
+
+function setIsRelativeScale(value) {
+  isRelativeScale = value || false;
 }
 
 function getNodes() {
   return figma.currentPage.children;
 }
 
-function getMapCorners(nodes) {
+function setMapCorners(nodes) {
+  if (!isRelativeScale) {
+    corners = DEFAULT_CORNERS;
+    biggerCorner = MAX_SPACE;
+    return;
+  }
+
   let [x1, y1, x2, y2] = [0, 0, 0, 0];
 
   nodes.forEach((node) => {
@@ -27,40 +53,48 @@ function getMapCorners(nodes) {
 
     if (xStart < x1) x1 = xStart;
     if (xEnd > x2) x2 = xEnd;
-    if (yStart < y1) x1 = xStart;
+    if (yStart < y1) y1 = yStart;
     if (yEnd > y2) y2 = yEnd;
   });
 
-  return [Math.round(x1), Math.round(y1), Math.round(x2), Math.round(y2)];
-}
-
-function getScale([x1, y1, x2, y2]: number[]) {
   const width = Math.abs(x2 - x1);
   const height = Math.abs(y2 - y1);
-  return Math.round(((width > height ? width : height) / MAX_SPACE / FRAME_SIZE) * 1000) / 1000 || 0.001;
+
+  corners = [x1, y1, x2, y2];
+  biggerCorner = width > height ? width : height;
 }
 
-function calcNodesPreview(nodes, scale) {
+function getShift(x, y) {
+  const [x0, y0] = corners;
+
+  const shiftedX = isRelativeScale ? x - x0 : x + MAX_SPACE / 2;
+  const shiftedY = isRelativeScale ? y - y0 : y + MAX_SPACE / 2;
+
+  return [shiftedX, shiftedY];
+}
+
+function calcNodesPreview(nodes) {
   return nodes.map((node) => {
     const { id, x, y, width, height } = node;
+
+    const [shiftX, shiftY] = getShift(x, y);
+
     return {
       id,
-      x: Math.ceil((x + MAX_SPACE / 2) * scale),
-      y: Math.ceil((y + MAX_SPACE / 2) * scale),
+      x: Math.ceil(shiftX * scale),
+      y: Math.ceil(shiftY * scale),
       width: Math.round(width * scale) || 1,
       height: Math.round(height * scale) || 1,
     };
   });
 }
 
-function updateMap(relativeScale = false) {
+function updateMap() {
   const nodes = getNodes();
-  const corners = getMapCorners(nodes);
-  const scale = getScale(corners);
-  const nodesPreview = calcNodesPreview(nodes, scale);
+  setMapCorners(nodes);
+  setScale(biggerCorner);
+  const nodesPreview = calcNodesPreview(nodes);
   figma.ui.postMessage({ type: 'updateMap', data: nodesPreview });
-  //getCurrentView
-  //sendMapData
 }
 
 figma.showUI(__html__, {
@@ -70,10 +104,10 @@ figma.showUI(__html__, {
 
 function updateSelectionView() {
   const { x, y, width, height } = figma.viewport.bounds;
-  const scale = 0.001;
+  const [shiftX, shiftY] = getShift(x, y);
   const data = {
-    left: Math.ceil((x + MAX_SPACE / 2) * scale),
-    top: Math.ceil((y + MAX_SPACE / 2) * scale),
+    left: Math.ceil(shiftX * scale),
+    top: Math.ceil(shiftY * scale),
     width: Math.round(width * scale) || 1,
     height: Math.round(height * scale) || 1,
   };
