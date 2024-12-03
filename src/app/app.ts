2fecbaef3468042baf224f40f40f09bde0938208
Roman Nebel <im.roman.nebel@gmail.com>
2024-12-03 16:43:39 +0100

    Add payment gateway

--- src/app/app.ts
+++ src/app/app.ts
@@ -5,12 +5,7 @@ const DEFAULT_CORNERS = [-131071, -131071, 131071, 131071];
 let corners = DEFAULT_CORNERS;
 let biggerCorner = MAX_SPACE;
 let scale = 0;
-let isRelativeScale = false;
-
-figma.showUI(__html__, {
-  width: 262,
-  height: 262,
-});
+let isRelativeScale = true;
 
 function uiMessageHandler(message) {
   //console.log('Message from UI:', message);
@@ -148,22 +143,166 @@ function updateSelectionView() {
   figma.ui.postMessage({ type: 'updateSelectionView', data });
 }
 
-figma.on('currentpagechange', updateMap);
+const { showNotify, closeNotify } = (() => {
+  let notify: NotificationHandler | null = null;
 
-figma.loadAllPagesAsync().then(() => {
-  figma.on('documentchange', (event) => {
-    const hasGeometryChange = event.documentChanges.find((change) => {
-      return (
-        change.type === 'CREATE' ||
-        change.type === 'DELETE' ||
-        (change.type === 'PROPERTY_CHANGE' &&
-          change.properties.some((prop) => ['x', 'y', 'width', 'height'].includes(prop)))
-      );
+  function closeNotify() {
+    if (notify) {
+      notify.cancel();
+      notify = null;
+    }
+  }
+
+  function showNotify(text: string, settings: NotificationOptions) {
+    closeNotify();
+    setTimeout(() => (notify = figma.notify(text, settings)), 10);
+  }
+
+  return { showNotify, closeNotify };
+})();
+
+async function checkSubscription(email?: string) {
+  const userId = figma.currentUser?.id || '';
+  let uri = `https://mcrprdcts.eduhund.com/api/shlow/check_subscription?user_id=${userId}`;
+
+  if (email) {
+    uri += `&email=${email}`;
+  }
+  try {
+    const response = await fetch(uri, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
     });
-    if (hasGeometryChange) updateMap();
+
+    if (response.status !== 200) {
+      showNotify('We have some problem. Please, run plugin again, or mail us: we@eduhund.com', {
+        error: true,
+        timeout: 10 * 1000,
+        onDequeue: () => {
+          figma.closePlugin();
+        },
+      });
+      return null;
+    }
+
+    const data = await response.json();
+    return data?.access;
+  } catch {
+    console.error('Subscribtion check error:');
+    figma.closePlugin();
+  }
+}
+
+async function run() {
+  figma.showUI(__html__, {
+    width: 262,
+    height: 262,
+    visible: false,
   });
-});
 
-figma.ui.onmessage = uiMessageHandler;
+  figma.ui.onmessage = uiMessageHandler;
 
-setInterval(updateSelectionView, 100);
+  showNotify('Cheking your subscription...', {
+    timeout: Infinity,
+  });
+
+  const result = await checkSubscription();
+
+  if (result === null) {
+    return;
+  }
+
+  if (false) {
+    closeNotify();
+    figma.ui.postMessage({
+      type: 'SET_STATUS',
+      data: {
+        status: 'full',
+      },
+    });
+  } else {
+    const timeFromFirstRun = figma?.payments?.getUserFirstRanSecondsAgo() || 0;
+    const trialTime = 7;
+    const timeRemaining = trialTime - Math.ceil(timeFromFirstRun / (24 * 60 * 60));
+
+    if (true || timeRemaining < 1) {
+      showNotify(`You have reached ${trialTime} days free trial`, {
+        timeout: Infinity,
+        button: {
+          text: 'Get full version',
+          action: () => {
+            figma.ui.postMessage({
+              type: 'SET_PAGE',
+              data: {
+                page: 'buy',
+              },
+            });
+          },
+        },
+        onDequeue: (reason) => {
+          if (reason !== 'action_button_click') {
+            closeNotify();
+            figma.closePlugin();
+          }
+        },
+      });
+      figma.ui.postMessage({
+        type: 'SET_STATUS',
+        data: {
+          status: 'trialEnded',
+        },
+      });
+    } else {
+      showNotify(`Welcome to MAPPE trial (${trialTime} day${trialTime > 1 ? 's' : ''} remaining)`, {
+        timeout: Infinity,
+        button: {
+          text: 'Get full version',
+          action: () => {
+            figma.ui.postMessage({
+              type: 'SET_PAGE',
+              data: {
+                page: 'buy',
+              },
+            });
+          },
+        },
+        onDequeue: (reason) => {
+          if (reason !== 'action_button_click') {
+            closeNotify();
+            figma.closePlugin();
+          }
+        },
+      });
+      figma.ui.postMessage({
+        type: 'SET_STATUS',
+        data: {
+          status: 'trial',
+        },
+      });
+    }
+  }
+
+  figma.loadAllPagesAsync().then(() => {
+    figma.on('documentchange', (event) => {
+      const hasGeometryChange = event.documentChanges.find((change) => {
+        return (
+          change.type === 'CREATE' ||
+          change.type === 'DELETE' ||
+          (change.type === 'PROPERTY_CHANGE' &&
+            change.properties.some((prop) => ['x', 'y', 'width', 'height'].includes(prop)))
+        );
+      });
+      if (hasGeometryChange) updateMap();
+    });
+  });
+
+  figma.on('currentpagechange', updateMap);
+
+  setInterval(updateSelectionView, 100);
+
+  figma.ui.show();
+}
+
+run();
--- src/ui/components/App.tsx
+++ src/ui/components/App.tsx
@@ -1,15 +1,20 @@
 import React, { useEffect, useRef, useState } from 'react';
 import '../styles/ui.css';
-import { Switch } from 'antd';
+import { Button, Input, Tooltip, Typography, Space, Switch } from 'antd';
+import { ExclamationCircleOutlined } from '@ant-design/icons';
+
+const { Title, Text, Link } = Typography;
 import { nodesData } from '../../types';
 
-function App() {
+function Map({ navTo }) {
   const mapRef = useRef(null);
 
   const [frames, setFrames] = useState<nodesData | null>(null);
 
   const [view, setView] = useState({ left: 0, top: 0, width: 0, height: 0 });
-  const [relative, setRelative] = useState(false);
+  const [relative, setRelative] = useState(true);
+
+  const [status, setStatus] = useState('trial');
 
   function relativeScaleSwitchHandler() {
     setRelative(!relative);
@@ -67,6 +72,10 @@ function App() {
       if (type === 'updateSelectionView') {
         setView({ ...view, ...data });
       }
+
+      if (type === 'SET_STATUS') {
+        setStatus(data.status);
+      }
     };
 
     window.addEventListener('message', handleMessage);
@@ -80,14 +89,165 @@ function App() {
 
   return (
     <>
-      <div id="mapContainer">
+      <div id="mapContainer" className={status === 'trialEnded' ? ' _blurred' : ''}>
         <div id="currentView" style={view}></div>
         <canvas id="map" ref={mapRef} width="262" height="262"></canvas>
       </div>
-      <label id="relativeScaleLabel" htmlFor="relativeScale">
-        <Switch id="relativeScale" size="small" checked={relative} onChange={relativeScaleSwitchHandler} />
-        Relative scale
-      </label>
+      {status !== 'trialEnded' && (
+        <label className="mapLabel" id="relativeScaleLabel" htmlFor="relativeScale">
+          <Switch id="relativeScale" size="small" checked={relative} onChange={relativeScaleSwitchHandler} />
+          Relative scale
+        </label>
+      )}
+      {status !== 'full' && (
+        <div className={'mapLabel' + (status === 'trialEnded' ? ' trialEnded' : '')} id="buy">
+          <Button
+            size={status === 'trialEnded' ? 'middle' : 'small'}
+            type={status === 'trialEnded' ? 'primary' : 'link'}
+            onClick={() => navTo('buy')}
+          >
+            Get full version
+          </Button>
+        </div>
+      )}
+    </>
+  );
+}
+
+function Buy({ navTo }) {
+  const [emailValue, setEmailValue] = useState(null);
+  const [isEmailValid, setIsEmailValid] = useState(null);
+  const [isChecking, setIsChecking] = useState(false);
+
+  function checkSubscription(e) {
+    e.preventDefault();
+    setIsEmailValid(null);
+    setIsChecking(true);
+    parent.postMessage(
+      {
+        pluginMessage: {
+          type: 'CHECK_EMAIL',
+          email: emailValue,
+        },
+      },
+      '*'
+    );
+  }
+
+  function emailValueHandler(value) {
+    setIsEmailValid(null);
+    setEmailValue(value);
+  }
+
+  return (
+    <div className="content">
+      <Button type="link" onClick={() => navTo('map')}>
+        Back
+      </Button>
+      <div className="block features">
+        <Title level={3}>What will you get?</Title>
+        <ul>
+          <li>
+            <Text>Full access to the plugin features</Text>
+          </li>
+          <li>
+            <Text>All future updates</Text>
+          </li>
+        </ul>
+      </div>
+      <div className="block instruction">
+        <Title level={3}>Steps to buy</Title>
+        <ol>
+          <li>
+            <Text>
+              Go to the <Link href="https://eduhund.gumroad.com/l/mappe">MAPPE page</Link> on Gumroad and join the
+              monthly subscription. In the order provide a valid email and your name to continue
+            </Text>
+          </li>
+          <li>
+            <Text>
+              Wait 1-2 minutes (while we receive your subscription details) and enter the email you provided on Gumroad
+            </Text>
+          </li>
+          <div>
+            <Space.Compact style={{ width: '100%' }}>
+              <Input
+                type="email"
+                value={emailValue}
+                placeholder="your@email.here"
+                status={isEmailValid === false ? 'error' : null}
+                disabled={isChecking}
+                suffix={
+                  isEmailValid === false ? (
+                    <Tooltip title="We didn't find your email. Please, check it or contact us.">
+                      <ExclamationCircleOutlined style={{ color: 'red' }} />
+                    </Tooltip>
+                  ) : null
+                }
+                onChange={({ target }) => emailValueHandler(target.value)}
+                onPressEnter={checkSubscription}
+              />
+              <Button
+                type="primary"
+                danger={isEmailValid === false}
+                loading={isChecking}
+                disabled={isEmailValid === false}
+                onClick={checkSubscription}
+              >
+                {isEmailValid === false ? 'Invalid email' : 'Check'}
+              </Button>
+            </Space.Compact>
+          </div>
+        </ol>
+        <Text>When succeed, this modal will close and you can start using the plugin.</Text>
+        <Text>
+          Please <a href="mailto:we@eduhund.com">contact us</a> if you experience any problems.
+        </Text>
+      </div>
+      <footer className="main_footer">
+        <Text>
+          <Link href="https://nebel.im" underline>
+            Roman Nebel
+          </Link>{' '}
+          from{' '}
+          <Link href="https://eduhund.com" underline>
+            eduHund
+          </Link>
+        </Text>
+        <nav className="footer_nav">
+          <Text>
+            <Link underline href="mailto:we@eduhund.com">
+              Mail us
+            </Link>
+          </Text>
+        </nav>
+      </footer>
+    </div>
+  );
+}
+
+function App() {
+  const [page, setPage] = useState('map');
+
+  useEffect(() => {
+    const handleMessage = (event) => {
+      const { type, data } = event.data.pluginMessage;
+
+      if (type === 'SET_PAGE') {
+        const { page } = data;
+        setPage(page);
+      }
+    };
+
+    window.addEventListener('message', handleMessage);
+
+    return () => window.removeEventListener('message', handleMessage);
+  }, []);
+
+  return (
+    <>
+      {page === 'map' && <Map navTo={setPage} />}
+      {page === 'buy' && <Buy navTo={setPage} />}
     </>
   );
 }
